0ac76798075d4b446480e6b2c14e549aeb5d037b
soimon <soimon@soimon.com>
2023-12-07 16:11:21 +0100

    Merge the properties with the actual page

--- src/notion/retrieval.ts
+++ src/notion/retrieval.ts
@@ -1,19 +1,23 @@
 import {Client} from '@notionhq/client';
 import {PropertiesList, GetPropertiesResult, getProperties} from './page-utils';
+import {PageObjectResponse} from '@notionhq/client/build/src/api-endpoints';
 
-// TODO: merge the output type with the actual API output, only replace the properties by a typed version
-export async function retrievePage<TList extends PropertiesList>(options: {
+export async function retrievePage<
+	TPropertiesList extends PropertiesList,
+>(options: {
 	id: string;
 	notion: Client;
-	properties: TList;
-}): Promise<GetPropertiesResult<TList> | undefined> {
+	properties: TPropertiesList;
+}): Promise<NotionPage<TPropertiesList> | undefined> {
 	const response = await options.notion.pages.retrieve({
 		page_id: options.id,
 		filter_properties: getPropertyIds(options.properties),
 	});
 	if (!('url' in response)) return undefined;
-	return getProperties(response, options.properties);
+	const properties = getProperties(response, options.properties);
+	return {...response, properties};
 }
+
 const getPropertyIds = (properties: PropertiesList) => {
 	const ids = Object.entries(properties).map(([, data]) =>
 		typeof data === 'object' ? data.id : null
@@ -21,4 +25,11 @@ const getPropertyIds = (properties: PropertiesList) => {
 	const hasOnlyIds = (list: typeof ids): list is string[] =>
 		!list.some(i => typeof i !== 'string');
 	return hasOnlyIds(ids) ? ids : undefined;
+};
+
+type NotionPage<TPropertiesList extends PropertiesList> = Omit<
+	PageObjectResponse,
+	'properties'
+> & {
+	properties: GetPropertiesResult<TPropertiesList>;
 };
