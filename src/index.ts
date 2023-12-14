03f2d050a3b21873933655cd0f0213bdb92947ad
soimon <soimon@soimon.com>
2023-12-14 19:48:09 +0100

    ⚰️ Clean up the main file

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ import {log} from './framework/utils/dev';
 import {NotionProjectRepository} from './project/notion/repositories/projects';
 import {RepositorySyncer} from './project/syncers/repository';
 import {TodoistProjectRepository} from './project/todoist/repositories/projects';
-import {getDatabaseSchema} from '@lib/notion';
 dotenv.config();
 console.clear();
 
@@ -52,54 +51,6 @@ async function main() {
 	// Sync projects
 
 	sync({projects: projectStrategy, tasks: taskStrategy}, notion, todoist);
-
-	// Fetch notion tasks
-
-	// TODO: Get all potentially out of sync candidates from the Notion side
-	// - Tasks that are open, and not in Todoist (closed isn't visible anyway)
-	// - Tasks that have recently changed
-	// - Tasks that have recently been added
-	// This covers the following Notion -> Todoist sync cases:
-	// - Task is created in Notion
-	// - Task is updated in Notion
-	// - Task is closed in Notion
-	// - Task is reopened in Notion
-	// - Task is scheduled in Notion
-	// - Task is unscheduled in Notion
-	// - Task is moved to a different goal in Notion
-	// This doesn't cover the following sync cases:
-	// - Task is deleted in Notion
-	// - Task is moved to a different project in Notion
-	// - Goal or project properties have changed
-	// So in order to cover those cases, we need to also:
-	// - Get all open tasks from Todoist (to see if they're not deleted)
-
-	// const notionCandidates = await notionTasks.getSyncCandidates(new Date());
-	// const todoistCandidates = await todoistTasks.getSyncCandidates();
-	// const diff = diffCandidates(notionCandidates, todoistCandidates);
-	// console.log(diff.notion);
-
-	// Update tasks in Todoist
-	// for (const notionTask of changedTasks) {
-	// 	const wasFound = await todoistTasks.update(notionTask.todoistId, {
-	// 		content: notionTask.content,
-	// 		description: notionTask.projectName,
-	// 		isCompleted: notionTask.isCompleted,
-	// 		dueDate: notionTask.scheduled,
-	// 	});
-	// 	if (!wasFound) newTasks.push(notionTask);
-	// }
-
-	// // Create tasks in Todoist
-
-	// newTasks.forEach(async t => {
-	// 	const todoistId = await todoistTasks.add({
-	// 		content: t.content,
-	// 		description: t.projectName,
-	// 		dueDate: t.scheduled,
-	// 	});
-	// 	await notionTasks.linkWithTodoist(t.notionId, todoistId);
-	// });
 }
 
 main();
@@ -109,11 +60,6 @@ function createRepositories(): {notion: NotionRepos; todoist: TodoistRepos} {
 		auth: process.env.NOTION_TOKEN,
 	});
 	const todoistApi = new TodoistApi(process.env.TODOIST_TOKEN);
-
-	// getDatabaseSchema({
-	// 	notion: notionApi,
-	// 	database: process.env.NOTION_DB_TASKS,
-	// }).then(console.log);
 
 	return {
 		notion: {
